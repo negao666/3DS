01d602b88faa052c05374ff991318fdbf1ff989f
negao666 <gabrielrossa336@gmail.com>
2025-02-14 10:28:16 -0300

    Add files via upload

---
+++ PRATICA001_VNICIUS_ROSSA/app.js
@@ -0,0 +1,15 @@
+//Declaração de variável
+let nome ="Emengarda";
+//console.log(nome);
+
+function add(a,b){
+    return a+b;
+}
+//console.log(add(7,9));
+
+//console.log(global);
+
+
+setTimeout(()=>{
+    console.log("A casa caiu mano")
+}, 3000);
